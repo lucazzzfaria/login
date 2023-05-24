9f1b88e7c4c0f7e4f2517ee638d2138df160f626
Lucas <lucas.nunesmf@gmail.com>
2023-05-24 14:06:12 -0300

    login

---
+++ src/app/components/admin/pages/home/home.component.ts
@@ -0,0 +1,15 @@
+import { AuthService } from 'src/app/core/services/auth.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {
+  constructor(private authService: AuthService) {}
+
+  public logout() {
+    this.authService.logout();
+  }
+}
---
+++ src/app/core/components/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { SignComponent } from './pages/sign/sign.component';
+
+
+@NgModule({
+  declarations: [
+    SignComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    ReactiveFormsModule
+  ]
+})
+export class AuthModule { }
